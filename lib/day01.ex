9af7f48e7883e826ce6a2d32ab17ca7040dfa535
Arturo Herrero <arturo.herrero@gmail.com>
2018-12-03 00:34:21 +0000

    Day 01

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,18 +1,32 @@
+# --- Day 1: Chronal Calibration ---
 defmodule Day01 do
-  @moduledoc """
-  Documentation for Day01.
-  """
+  def final_frequency(stream) do
+    stream
+    |> map_to_integer
+    |> Enum.sum
+  end
 
-  @doc """
-  Hello world.
+  def frequency_reached_twice(stream) do
+    stream
+    |> map_to_integer
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, []}, fn x, {current_frequency, history_frequencies} ->
+      new_frequency = current_frequency + x
 
-  ## Examples
+      if Enum.member?(history_frequencies, new_frequency) do
+        {:halt, new_frequency}
+      else
+        {:cont, {new_frequency, [new_frequency | history_frequencies]}}
+      end
+    end)
+  end
 
-      iex> Day01.hello()
-      :world
+  defp map_to_integer(stream) do
+    stream
+    |> Stream.map(fn line -> String.trim(line) |> String.to_integer end)
+  end
 
-  """
-  def hello do
-    :world
+  defp file_stream do
+    File.stream!("input/day01.txt", [], :line)
   end
 end
--- test/day01_test.exs
+++ test/day01_test.exs
@@ -1,8 +1,25 @@
 defmodule Day01Test do
   use ExUnit.Case
-  doctest Day01
 
-  test "greets the world" do
-    assert Day01.hello() == :world
+  test "final_frequency/1" do
+    stream = [
+      "+1\n",
+      "+1\n",
+      "-1\n",
+    ]
+
+    assert Day01.final_frequency(stream) == 1
+  end
+
+  test "frequency_reached_twice/1" do
+    stream = [
+     "+3\n",
+     "+3\n",
+     "+4\n",
+     "-2\n",
+     "-4\n",
+    ]
+
+    assert Day01.frequency_reached_twice(stream) == 10
   end
 end
