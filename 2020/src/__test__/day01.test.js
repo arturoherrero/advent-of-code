9c87a1abd35e562937eef2e5464918ed853d6a37
Arturo Herrero <arturo.herrero@gmail.com>
2021-02-21 23:58:14 +0000

    2020: Day 1. JavaScript

---
+++ 2020/src/__test__/day01.test.js
@@ -0,0 +1,27 @@
+const { findTwoSum2020AndMultiply, findThreeSum2020AndMultiply } = require('../day01');
+
+test('findTwoSum2020AndMultiply', () => {
+  expect(findTwoSum2020AndMultiply(
+    [
+      '1721',
+      '979',
+      '366',
+      '299',
+      '675',
+      '1456',
+    ],
+  )).toBe(514579);
+});
+
+test('findThreeSum2020AndMultiply', () => {
+  expect(findThreeSum2020AndMultiply(
+    [
+      '1721',
+      '979',
+      '366',
+      '299',
+      '675',
+      '1456',
+    ],
+  )).toBe(241861950);
+});
---
+++ 2020/src/day01.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+const fs = require('fs');
+
+const input = fs.readFileSync('input/day01.txt').toString('utf-8');
+const inputArray = input.split('\n');
+
+const findTwoSum2020AndMultiply = (array) => {
+  loop:
+  for (let i = 0; i < array.length; i += 1) {
+    var entry1 = Number(array[i]);
+
+    for (let j = i + 1; j < array.length; j += 1) {
+      var entry2 = Number(array[j]);
+
+      if (entry1 + entry2 === 2020) {
+        break loop;
+      }
+    }
+  }
+
+  return entry1 * entry2;
+};
+
+const findThreeSum2020AndMultiply = (array) => {
+  loop:
+  for (let i = 0; i < array.length; i += 1) {
+    var entry1 = Number(array[i]);
+
+    for (let j = i + 1; j < array.length; j += 1) {
+      var entry2 = Number(array[j]);
+
+      for (let k = j + 1; k < array.length; k += 1) {
+        var entry3 = Number(array[k]);
+
+        if (entry1 + entry2 + entry3 === 2020) {
+          break loop;
+        }
+      }
+    }
+  }
+
+  return entry1 * entry2 * entry3;
+};
+
+module.exports = { findTwoSum2020AndMultiply, findThreeSum2020AndMultiply };
